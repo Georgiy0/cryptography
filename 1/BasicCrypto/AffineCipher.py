ef193028c947f31a122dd2a06b341d3744d5b335
Kubrin Georgiy <kubrin.eg.s@gmail.com>
2018-08-19 18:00:01 +0300

    Change the key in ShiftCipher to be the value of the shift.

--- 1/BasicCrypto/AffineCipher.py
+++ 1/BasicCrypto/AffineCipher.py
@@ -2,10 +2,13 @@ class ShiftCipher:
 	def __init__(self, alphabet, key):
 		self.key = self.processKey(key)
 		self.alphabet = alphabet
+		if self.key not in self.alphabet:
+			raise ValueError('Provided key does not belong to the alphabet')
 		self.modulo = len(alphabet)
 		self.alphabet_map = {}
 		for i in range(0, self.modulo):
 			self.alphabet_map[self.alphabet[i]] = i
+		self.key_pos = self.alphabet_map[self.key]
 		
 	def processKey(self, key):
 		if isinstance(key, int):
@@ -23,7 +26,7 @@ class ShiftCipher:
 		for byte in data:
 			if byte not in self.alphabet:
 				raise ValueError("Alphabet of the data doesn't match with the provided alphabet")
-			encrypted_data += bytes([self.alphabet[(self.alphabet_map[byte] + self.key) % self.modulo]])
+			encrypted_data += bytes([self.alphabet[(self.alphabet_map[byte] + self.key_pos) % self.modulo]])
 		return encrypted_data
 			
 	def decrypt(self, data):
@@ -31,5 +34,5 @@ class ShiftCipher:
 		for byte in data:
 			if byte not in self.alphabet:
 				raise ValueError("Alphabet of the data doesn't match with the provided alphabet")
-			decrypted_data += bytes([self.alphabet[(self.alphabet_map[byte] - self.key) % self.modulo]])
+			decrypted_data += bytes([self.alphabet[(self.alphabet_map[byte] - self.key_pos) % self.modulo]])
 		return decrypted_data
